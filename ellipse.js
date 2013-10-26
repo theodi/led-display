8fec9e8d1fd68c2f4d53936c9813435f37d3cb73
Jeni Tennison <jeni@theodi.org>
2013-10-26 21:05:56 +0100

    fixes to mountains, plus everyone sequence

--- ellipse.js
+++ ellipse.js
@@ -2,21 +2,32 @@ $(document).ready(function () {
   $('.display').each(function () {
     var 
       $this = $(this),
-      showAt = $this.attr('data-display-at'),
-      hideAt = $this.next('.display').eq(0).attr('data-display-at');
+      $next = $this.next('.display').eq(0),
+      showAt = $this.attr('data-display-at') * 1000,
+      hideAt = $next.attr('data-display-at') * 1000,
+      effect = ($this.hasClass('up') || $this.hasClass('right')) ? 'slide' : 'fade',
+      duration = ($this.attr('data-transition') || 0) * 1000,
+      nextDuration = $next.attr('data-transition') * 1000,
+      direction = $this.hasClass('up') ? 'down' : 'right',
+      showOpts = {
+        effect: effect,
+        duration: duration,
+        direction: direction
+      },
+      hideOpts = {
+        effect: effect,
+        duration: nextDuration,
+        direction: direction === 'down' ? 'up' : 'left'
+      };
+    console.log(showOpts);
+    console.log(hideOpts);
     $this
       .delay(showAt)
-      .show({
-        effect: 'fade',
-        duration: 10000
-      });
-    if ($this.next('.display').length > 0) {
+      .show(showOpts);
+    if ($next.length > 0) {
       $this
-        .delay(hideAt - showAt)
-        .hide({
-          effect: 'fade',
-          duration: 10000
-        });
+        .delay(hideAt - showAt - duration)
+        .hide(hideOpts);
     }
   });
 });
