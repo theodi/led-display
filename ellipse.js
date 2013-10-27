ee9f6cb5a2e6d82899c8682005f42a527128b56f
Jeni Tennison <jeni@theodi.org>
2013-10-27 23:41:38 +0000

    more scripting etc

--- ellipse.js
+++ ellipse.js
@@ -1,33 +1,65 @@
 $(document).ready(function () {
+  var marquee = function ($display, showOpts, hideOpts, loop) {
+    if (loop > 0) {
+      hideOpts.complete = marquee($display, showOpts, hideOpts, loop - 1);
+      return function () {
+        console.log('showing & hiding');
+        console.log($display);
+        $display
+          .show(showOpts)
+          .hide(hideOpts);
+      };
+    }
+  };
   $('.display').each(function () {
     var 
       $this = $(this),
       $next = $this.next('.display').eq(0),
-      showAt = $this.attr('data-display-at') * 100,
-      hideAt = $next.attr('data-display-at') * 100,
+      showAt = $this.attr('data-display-at') * 1000,
+      hideAt = $next.attr('data-display-at') * 1000,
       effect = ($this.hasClass('up') || $this.hasClass('right') || $this.hasClass('down')) ? 'slide' : 'fade',
       nextEffect = ($next.hasClass('up') || $next.hasClass('right') || $next.hasClass('down')) ? 'slide' : 'fade',
       duration = ($this.attr('data-transition') || 0) * 1000,
       nextDuration = $next.attr('data-transition') * 1000,
       direction = $this.hasClass('up') ? 'down' : $this.hasClass('down') ? 'up' : 'right',
       nextDirection = $next.hasClass('up') ? 'up' : $next.hasClass('down') ? 'down' : 'left',
+      easing = $this.hasClass('right') ? 'linear' : 'swing',
+      nextEasing = $next.hasClass('right') ? 'linear' : 'swing',
       showOpts = {
         effect: effect,
         duration: duration,
-        direction: direction
+        direction: direction,
+        easing: easing
       },
       hideOpts = {
         effect: nextEffect,
         duration: nextDuration,
-        direction: nextDirection
+        direction: nextDirection,
+        easing: nextEasing
       };
-    $this
-      .delay(showAt)
-      .show(showOpts);
-    if ($next.length > 0) {
+    if ($this.hasClass('multiline')) {
+      $this.show();
+    } else {
       $this
-        .delay(hideAt - showAt - duration)
-        .hide(hideOpts);
+        .delay(showAt)
+        .show(showOpts);
+      if ($this.hasClass('line')) {
+        hideOpts = {
+            effect: 'slide',
+            duration: duration,
+            direction: 'left',
+            easing: 'linear'
+        };
+        // hideOpts.complete = marquee($this, showOpts, hideOpts, 10);
+        console.log('hiding');
+        console.log($this);
+        $this
+          .hide(hideOpts);
+      } else if ($next.length > 0) {
+        $this
+          .delay(hideAt - showAt - duration)
+          .hide(hideOpts);
+      }
     }
   });
 });
